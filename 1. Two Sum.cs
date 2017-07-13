007a410ba4a56920d310e3d51b2f24330e625e91
menghuizhao <menghui524@gmail.com>
2017-07-12 22:01:57 -0700

    the first one

---
+++ 1. Two Sum.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        if(nums == null || nums.Length < 1)
+        {
+            return null;
+        }
+        for(int i = 0; i < nums.Length; i++)
+        {
+            for(int j = i + 1; j < nums.Length; j++)
+            {
+                if(nums[i] + nums[j] == target)
+                {
+                    return new int[2]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+}
