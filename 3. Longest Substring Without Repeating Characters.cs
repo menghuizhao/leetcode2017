f43ba10c26a283bcaf79d77dea281c0ad20fc25f
Mengzhe Zhao <menghui524@gmail.com>
2017-09-11 22:15:08 -0700

    Question 3

---
+++ 3. Longest Substring Without Repeating Characters.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public int LengthOfLongestSubstring(string s) {
+        if(string.IsNullOrEmpty(s)) return 0;
+        int maxLength = 1;
+        int tempLength = 1;
+        Dictionary<char, int> dict = new Dictionary<char, int>();//key: character, value: first index at string
+        for(int i = 0; i < s.Length; i++){
+            if(i == 0){
+                dict[s[0]] = 0;   
+            }
+            for(int j = i + 1; j < s.Length; j++){
+                //condition: 1. has duplicate char 2. duplicate char in range [i,j]
+                if(dict.ContainsKey(s[j]) && dict[s[j]] >= i && dict[s[j]] < j){
+                    
+                    i = dict[s[j]];
+                    dict[s[j]] = j;
+                    break;
+                }
+                else{
+                    dict[s[j]] = j;
+                    tempLength = j - i + 1;
+                    if(tempLength > maxLength){
+                        maxLength = tempLength;
+                    }
+                }
+            }
+        }
+        return maxLength;
+    }
+}
