fea41df1db569ff4197b131e853f8095c6c05297
Mengzhe Zhao <menghui524@gmail.com>
2017-09-14 00:25:58 -0700

    change line wrapping to fit format in jianshu

--- 3. Longest Substring Without Repeating Characters.cs
+++ 3. Longest Substring Without Repeating Characters.cs
@@ -3,15 +3,17 @@ public class Solution {
         if(string.IsNullOrEmpty(s)) return 0;
         int maxLength = 1;
         int tempLength = 1;
-        Dictionary<char, int> dict = new Dictionary<char, int>();//key: character, value: first index at string
+        //key: character, value: first index at string
+        Dictionary<char, int> dict = new Dictionary<char, int>();
         for(int i = 0; i < s.Length; i++){
             if(i == 0){
-                dict[s[0]] = 0;   
+                dict[s[0]] = 0;
             }
             for(int j = i + 1; j < s.Length; j++){
-                //condition: 1. has duplicate char 2. duplicate char in range [i,j]
+                //condition: 1. has duplicate char
+                //2. duplicate char in range [i,j]
                 if(dict.ContainsKey(s[j]) && dict[s[j]] >= i && dict[s[j]] < j){
-                    
+
                     i = dict[s[j]];
                     dict[s[j]] = j;
                     break;
