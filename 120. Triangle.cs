805e76a4f421f1afbaefef673df2a1e1befe6544
Mengzhe Zhao <menghui524@gmail.com>
2017-09-14 00:26:18 -0700

    question 120
    Needs refactoring

---
+++ 120. Triangle.cs
@@ -0,0 +1,42 @@
+public class Solution {
+    public int MinimumTotal(IList<IList<int>> triangle) {
+        //f(0) = 0
+        if(triangle == null || triangle.Count <= 0){
+            return 0;
+        }
+        //f(1) = 1
+        if(triangle.Count == 1){
+            return triangle[0][0];
+        }
+        //f(n) = f(n -2) + min{ GetElement(n - 1, i) + min(GetElement(n, i), GetElement(n, i + 1))}
+        int n = triangle.Count;
+        int[] current_row_min = new int[n];
+        int[] last_row_min = new int[n];
+        last_row_min[0] = triangle[0][0];
+        for(int level = 2; level <= n; level++){
+            for(int col = 1; col <= level; col ++){
+                if(col == 1){
+                    current_row_min[col - 1] = last_row_min[col - 1] + triangle[level - 1][col - 1];
+                }
+                else if(col < level){
+                    current_row_min[col - 1] = Math.Min(last_row_min[col - 2], last_row_min[col - 1]) + triangle[level - 1][col - 1];
+                }
+                else{
+                    current_row_min[col - 1] = last_row_min[col - 2] + triangle[level - 1][col - 1];
+                }
+                if(col - 2 >= 0) {
+                    last_row_min[col - 2] = current_row_min[col - 2]; //cache last row result
+                }
+                if(col == level){
+                    last_row_min[col - 1] = current_row_min[col - 1];
+                }
+
+            }
+        }
+        int temp_min = current_row_min[0];
+        for(int col = 1; col <= n; col++){
+            temp_min = Math.Min(temp_min, current_row_min[col - 1]);
+        }
+        return temp_min;
+    }
+}
